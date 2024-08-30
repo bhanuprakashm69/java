cccbf5636010dd2c3fbeb2abb79acb787ccf7203
bhanuprakashm69 <bhanuprakashm69o@gmail.com>
2024-08-30 08:27:21 +0530

    new file

--- Test.java.java
+++ Test.java.java
@@ -1,7 +1,9 @@
-class HelloWorld
-{
-public static void main (string args[])    
-{
-system.out.println("Hello World"); //prints on screen "Hello World"
-}
-}
+public class Test {
+
+      public static void main(string[] args) {
+             system.out.println("************************************************");
+             system .out.ptintln("Hello from GITHUB");
+             system.out.println("************************************************");        
+      }
+
+}             
